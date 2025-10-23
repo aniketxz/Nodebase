fc96b4328e9976f29ec196e550b5013a92295a76
Aniket Kumar Mahato <aniketkumar0x@gmail.com>
2025-10-23 23:00:06 +0530

    07: ai providers

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,19 +1,49 @@
-import prisma from "@/lib/db";
 import { inngest } from "./client";
+import { createGoogleGenerativeAI } from "@ai-sdk/google";
+import { createOpenAI } from "@ai-sdk/openai";
+import { createAnthropic } from "@ai-sdk/anthropic";
+import { generateText } from "ai";
 
-export const helloWorld = inngest.createFunction(
-	{ id: "hello-world" },
-	{ event: "test/hello.world" },
+const google = createGoogleGenerativeAI();
+const openai = createOpenAI();
+const anthropic = createAnthropic();
+
+export const execute = inngest.createFunction(
+	{ id: "execute-ai" },
+	{ event: "execute/ai" },
 	async ({ event, step }) => {
-		await step.sleep("fetching", "5s");
-		await step.sleep("transcribing", "5s");
-		await step.sleep("sending-to-ai", "5s");
-		return await step.run("create-workflow", () => {
-			return prisma.workflow.create({
-				data: {
-					name: "workflow-from-inngest",
-				},
-			});
-		});
+		await step.sleep("pretend", "5s");
+
+		const { steps: geminiSteps } = await step.ai.wrap(
+			"gemini-generate-text",
+			generateText,
+			{
+				model: google("gemini-2.5-flash"),
+				system: "You are a helpful assistant.",
+				prompt: "What is hono in web development",
+			}
+		);
+
+		const { steps: openaiSteps } = await step.ai.wrap(
+			"gemini-generate-text",
+			generateText,
+			{
+				model: openai("gpt-4"),
+				system: "You are a helpful assistant.",
+				prompt: "What is hono in web development",
+			}
+		);
+
+		const { steps: anthropicSteps } = await step.ai.wrap(
+			"gemini-generate-text",
+			generateText,
+			{
+				model: anthropic("claude-sonnet-4-5"),
+				system: "You are a helpful assistant.",
+				prompt: "What is hono in web development",
+			}
+		);
+
+		return { geminiSteps, openaiSteps, anthropicSteps };
 	}
 );
