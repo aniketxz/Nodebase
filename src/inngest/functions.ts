c8285f0b84ba8fbdabdf88fe6348cec911ef571e
Aniket Kumar Mahato <aniketkumar0x@gmail.com>
2026-03-17 21:27:49 +0530

    18: node execution

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,69 +1,46 @@
-import { generateText } from "ai";
-import { createOpenAI } from "@ai-sdk/openai";
-import { createAnthropic } from "@ai-sdk/anthropic";
-import { createGoogleGenerativeAI } from "@ai-sdk/google";
-import * as Sentry from "@sentry/nextjs";
+import { NonRetriableError } from "inngest";
 import { inngest } from "./client";
+import prisma from "@/lib/db";
+import { topologicalSort } from "./utils";
+import { NodeType } from "@/generated/prisma";
+import { getExecutor } from "@/features/executions/lib/executor-registry";
 
-const google = createGoogleGenerativeAI();
-const openai = createOpenAI();
-const anthropic = createAnthropic();
-
-export const execute = inngest.createFunction(
-	{ id: "execute-ai" },
-	{ event: "execute/ai" },
+export const executeWorkflow = inngest.createFunction(
+	{ id: "execute-workflow" },
+	{ event: "workflows/execute.workflow" },
 	async ({ event, step }) => {
-		await step.sleep("pretend", "5s");
+		const workflowId = event.data.workflowId;
 
-		Sentry.logger.info('User triggered test log', { log_source: 'sentry_test' });
-		console.warn("Something is missing");
-		console.error("This is an error i want to track");
+		if (!workflowId) {
+			throw new NonRetriableError("Workflow ID is missing");
+		}
 
-		const { steps: geminiSteps } = await step.ai.wrap(
-			"gemini-generate-text",
-			generateText,
-			{
-				model: google("gemini-2.5-flash"),
-				system: "You are a helpful assistant.",
-				prompt: "What is hono in web development",
-				experimental_telemetry: {
-					isEnabled: true,
-					recordInputs: true,
-					recordOutputs: true,
+		const sortedNodes = await step.run("prepare-workflow", async () => {
+			const workflow = await prisma.workflow.findUniqueOrThrow({
+				where: { id: workflowId },
+				include: {
+					nodes: true,
+					connections: true,
 				},
-			}
-		);
+			});
 
-		const { steps: openaiSteps } = await step.ai.wrap(
-			"gemini-generate-text",
-			generateText,
-			{
-				model: openai("gpt-4"),
-				system: "You are a helpful assistant.",
-				prompt: "What is hono in web development",
-				experimental_telemetry: {
-					isEnabled: true,
-					recordInputs: true,
-					recordOutputs: true,
-				},
-			}
-		);
+			return topologicalSort(workflow.nodes, workflow.connections);
+		});
 
-		const { steps: anthropicSteps } = await step.ai.wrap(
-			"gemini-generate-text",
-			generateText,
-			{
-				model: anthropic("claude-sonnet-4-5"),
-				system: "You are a helpful assistant.",
-				prompt: "What is hono in web development",
-				experimental_telemetry: {
-					isEnabled: true,
-					recordInputs: true,
-					recordOutputs: true,
-				},
-			}
-		);
+		// Initialize context with any initial data from the trigger
+		let context = event.data.initialData || {};
 
-		return { geminiSteps, openaiSteps, anthropicSteps };
-	}
+		// Execute each node
+		for (const node of sortedNodes) {
+			const executor = getExecutor(node.type as NodeType);
+			context = await executor({
+				data: node.data as Record<string, unknown>,
+				nodeId: node.id,
+				context,
+				step,
+			});
+		}
+
+		return { workflowId, result: context };
+	},
 );
