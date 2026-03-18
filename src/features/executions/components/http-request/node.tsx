19500afa290a05faa634d675e14bcddffc76534e
Aniket Kumar Mahato <aniketkumar0x@gmail.com>
2026-03-18 17:24:53 +0530

    21: node realtime status

--- src/features/executions/components/http-request/node.tsx
+++ src/features/executions/components/http-request/node.tsx
@@ -3,8 +3,11 @@
 import { memo, useState } from "react";
 import { GlobeIcon } from "lucide-react";
 import { Node, NodeProps, useReactFlow } from "@xyflow/react";
+import { HTTP_REQUEST_CHANNEL_NAME } from "@/inngest/channels/http-request";
 import { BaseExecutionNode } from "../base-execution-node";
 import { type HttpRequestFormValues, HttpRequestDialog } from "./dialog";
+import { useNodeStatus } from "../../hooks/use-node-status";
+import { fetchHttpRequestRealtimeToken } from "./actions";
 
 type HttpRequestNodeData = {
 	variableName?: string;
@@ -19,7 +22,12 @@ export const HttpRequestNode = memo((props: NodeProps<HttpRequestNodeType>) => {
 	const [dialogOpen, setDialogOpen] = useState(false);
 	const { setNodes } = useReactFlow();
 
-	const nodeStatus = "initial";
+	const nodeStatus = useNodeStatus({
+		nodeId: props.id,
+		channel: HTTP_REQUEST_CHANNEL_NAME,
+		topic: "status",
+		refreshToken: fetchHttpRequestRealtimeToken,
+	});
 
 	const handleOpenSettings = () => setDialogOpen(true);
 
@@ -31,7 +39,7 @@ export const HttpRequestNode = memo((props: NodeProps<HttpRequestNodeType>) => {
 						...node,
 						data: {
 							...node.data,
-							...values
+							...values,
 						},
 					};
 				}
